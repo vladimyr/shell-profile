02a19ebdd04fc03b90e6639bbaaec986f2672898
Dario Vladovic <d.vladimyr@gmail.com>
2019-01-06 00:56:48 +0100

    Implement shell profile module :tada:
    - synchronously get default shell profile path

---
+++ index.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const { execFileSync } = require('child_process');
+const { existsSync } = require('fs');
+const { resolve } = require('path');
+const mem = require('mem');
+
+const shellVar = name => name.startsWith('$') ? name : `$${name}`;
+
+/**
+ * Synchronously gets default shell profile path
+ * @name getProfilePath
+ * @returns {Promise<String?>} profile path
+ *
+ * @example
+ * const profile = require('shell-profile')();
+ * console.log('profile: %s', profile);
+ * //=> /Users/vladimyr/.zshrc
+ */
+module.exports = mem(function () {
+  const $HOME = require('os').homedir();
+  const [$BASH_VERSION, $ZSH_VERSION] = env('BASH_VERSION', 'ZSH_VERSION');
+  if (/^win/gi.test(process.platform)) return;
+  if ($BASH_VERSION) {
+    if (existsSync(resolve($HOME, '.bashrc'))) {
+      return resolve($HOME, '.bashrc');
+    } else if (existsSync(resolve($HOME, '.bash_profile'))) {
+      return resolve($HOME, '.bash_profile');
+    }
+  } else if ($ZSH_VERSION) {
+    if (existsSync(resolve($HOME, '.zshrc'))) return resolve($HOME, '.zshrc');
+  }
+  const profiles = ['.profile', '.bashrc', '.bash_profile', '.zshrc'];
+  return profiles.map(it => resolve($HOME, it)).find(it => existsSync(it));
+});
+
+function env(...vars) {
+  const shellVars = vars.map(it => shellVar(it));
+  const cmd = `echo -n "${shellVars.join(':')}"; exit;`;
+  const result = execFileSync(process.env.SHELL, ['-ilc', cmd], { encoding: 'utf8' });
+  return result.trim().split(':');
+}
---
+++ test.js
@@ -0,0 +1,76 @@
+'use strict';
+
+const { resolve } = require('path');
+const sinon = require('sinon');
+const test = require('tape');
+const rewiremock = require('rewiremock/node');
+
+const $HOME = require('os').homedir();
+
+test('detect zsh', t => {
+  rewiremock.enable();
+  const expected = resolve($HOME, '.zshrc');
+  const actual = rewiremock.proxy(() => require('./'), ({
+    child_process: { execFileSync: sinon.stub().returns(':5.2') },
+    fs: { existsSync: sinon.stub().callsFake(path => resolve(path) === expected) }
+  }));
+  t.plan(1);
+  t.equal(actual(), expected, `profile: ${expected}`);
+});
+
+test('detect zsh without `.zshrc`', t => {
+  rewiremock.enable();
+  const expected = resolve($HOME, '.profile');
+  const actual = rewiremock.proxy(() => require('./'), ({
+    child_process: { execFileSync: sinon.stub().returns(':5.2') },
+    fs: { existsSync: sinon.stub().callsFake(path => resolve(path) === expected) }
+  }));
+  t.plan(1);
+  t.equal(actual(), expected, `profile: ${expected}`);
+});
+
+test('detect bash with `.bashrc`', t => {
+  rewiremock.enable();
+  const expected = resolve($HOME, '.bashrc');
+  const actual = rewiremock.proxy(() => require('./'), ({
+    child_process: { execFileSync: sinon.stub().returns('3.2.57(1)-release:5.2') },
+    fs: { existsSync: sinon.stub().callsFake(path => resolve(path) === expected) }
+  }));
+  t.plan(1);
+  t.equal(actual(), expected, `profile: ${expected}`);
+});
+
+test('detect bash with `.bash_profile`', t => {
+  rewiremock.enable();
+  const expected = resolve($HOME, '.bash_profile');
+  const actual = rewiremock.proxy(() => require('./'), ({
+    child_process: { execFileSync: sinon.stub().returns('3.2.57(1)-release:') },
+    fs: { existsSync: sinon.stub().callsFake(path => resolve(path) === expected) }
+  }));
+  t.plan(1);
+  t.equal(actual(), expected, `profile: ${expected}`);
+});
+
+test('detect bash without _bash_-profile', t => {
+  rewiremock.enable();
+  const expected = resolve($HOME, '.profile');
+  const actual = rewiremock.proxy(() => require('./'), ({
+    child_process: { execFileSync: sinon.stub().returns('3.2.57(1)-release:') },
+    fs: { existsSync: sinon.stub().callsFake(path => resolve(path) === expected) }
+  }));
+  t.plan(1);
+  t.equal(actual(), expected, `profile: ${expected}`);
+});
+
+test('gracefull fallback to profile search', t => {
+  rewiremock.enable();
+  const expected = resolve($HOME, '.bashrc');
+  const actual = rewiremock.proxy(() => require('./'), ({
+    child_process: { execFileSync: sinon.stub().returns('') },
+    fs: { existsSync: sinon.stub().callsFake(path => resolve(path) === expected) }
+  }));
+  t.plan(1);
+  t.equal(actual(), expected, `profile: ${expected}`);
+});
+
+test.onFinish(() => rewiremock.disable());
